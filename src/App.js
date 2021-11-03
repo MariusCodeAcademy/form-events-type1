7e2da056a73a057b1aa9903a4cb7a002988bbfbd
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-11-03 09:56:42 +0200

    contactHtml

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import ContactForm from './components/ContactForm';
 import Counter from './components/Counter';
 
 function App() {
   return (
     <div className='App container'>
       <h1>Input forms</h1>
-      <Counter />
+      {/* <Counter /> */}
+      <ContactForm />
     </div>
   );
 }
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,3 +1,36 @@
+function ContactForm() {
+  return (
+    <div className='w-50 text-left'>
+      <form>
+        <input type='text' className='form-control' placeholder='Name' />
+        <input type='age' className='form-control' placeholder='Age' />
+        <select className='form-control' name='subject' id=''>
+          <option value='bills'>Bills</option>
+          <option value='information'>Information</option>
+          <option value='payments'>Payments</option>
+        </select>
+        <textarea
+          cols='30'
+          rows='10'
+          className='form-control'
+          placeholder='your text'
+        ></textarea>
+        <div className='form-check'>
+          <input className='form-check-input' type='checkbox' id='terms' />
+          <label className='form-check-label' htmlFor='terms'>
+            Terms and Conditions
+          </label>
+        </div>
+        <button className='btn btn-dark' type='submit'>
+          Send
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default ContactForm;
+
 // sukurti forma
 // inputs,
 // name, age,
