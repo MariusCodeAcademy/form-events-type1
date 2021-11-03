3a7b77542c329687b98d0915a151cce04cd1ecf4
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-11-03 09:08:13 +0200

    eventTarget

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,21 +3,33 @@ import { useState } from 'react';
 export default function Counter() {
   // press button to increase counter value
   const [counter, setCounter] = useState(0);
+  const [counterTitle, setCounterTitle] = useState('Cat');
 
   const onIncrement = () => {
     setCounter(counter + 1);
   };
 
+  const onTitleChange = () => {
+    console.log('title change');
+    // get value entered and update counterTitle
+    setCounterTitle('Dog');
+  };
+
   return (
     <div>
-      <h2>Counter</h2>
+      <h2>Counter: {counterTitle}</h2>
       <h3>{counter}</h3>
       <button onClick={onIncrement} className='btn btn-info'>
         inc
       </button>{' '}
       <br />
       <input type='text' placeholder='start value' /> <br />
-      <input type='text' placeholder='title' />
+      <input
+        type='text'
+        placeholder='title'
+        value={counterTitle}
+        onChange={onTitleChange}
+      />
     </div>
   );
 }
