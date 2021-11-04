b23256dcd77f3fc56edb71d2d04e24bd9593d52b
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-11-04 08:33:59 +0200

    counter state lift

--- src/components/CounterList.js
+++ src/components/CounterList.js
@@ -13,16 +13,42 @@ const initCounters = [
     counterValue: 5,
   },
 ];
+const initCounters2 = [
+  {
+    id: 1,
+    title: 'Dogs',
+    counterValue: 5,
+  },
+  {
+    id: 2,
+    title: 'Cats',
+    counterValue: 5,
+  },
+  {
+    id: 3,
+    title: 'Birds',
+    counterValue: 7,
+  },
+];
 
 function CounterList() {
   // get how many counters we have
   // get average counter ammount
   const [counters, setCounters] = useState(initCounters);
 
+  const handleIncrement = (id) => {
+    console.log('increse counter with id', id);
+    // take counters arr, find the one with id === id and indcrese counter value by 1
+  };
+
   return (
     <div className='d-flex'>
       {counters.map((counterObj) => (
-        <CounterSimple key={counterObj.id} title={counterObj.title} />
+        <CounterSimple
+          key={counterObj.id}
+          counter={counterObj}
+          onIcrement={handleIncrement}
+        />
       ))}
     </div>
   );
--- src/components/CounterSimple.js
+++ src/components/CounterSimple.js
@@ -1,18 +1,19 @@
 import { useState } from 'react';
 
-export default function CounterSimple(props) {
+export default function CounterSimple({ counter, onIcrement }) {
   // press button to increase counter value
-  const [counter, setCounter] = useState(0);
+  // const [counter, setCounter] = useState(0);
 
-  const onIncrement = () => {
-    setCounter(counter + 1);
+  const callIncrement = () => {
+    // setCounter(counter + 1);
+    onIcrement(counter.id);
   };
 
   return (
     <div className='border border-primary p-4 m-2'>
-      <h2>Counter: {props.title}</h2>
-      <h3>{counter}</h3>
-      <button onClick={onIncrement} className='btn btn-info'>
+      <h2>Counter: {counter.title}</h2>
+      <h3>{counter.counterValue}</h3>
+      <button onClick={callIncrement} className='btn btn-info'>
         inc
       </button>
     </div>
