e5fe573f47025a34d9a2465a29d88bc4ef0e9e1a
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-11-04 08:59:04 +0200

    add new counter

--- src/components/CounterList.js
+++ src/components/CounterList.js
@@ -24,33 +24,65 @@ const initCounters2 = [
     title: 'Cats',
     counterValue: 5,
   },
-  {
-    id: 3,
-    title: 'Birds',
-    counterValue: 7,
-  },
 ];
 
 function CounterList() {
   // get how many counters we have
   // get average counter ammount
   const [counters, setCounters] = useState(initCounters);
+  const [newCounterTitle, setNewCounterTitle] = useState('');
+  const onNewCounterTitleEntry = (e) => {
+    setNewCounterTitle(e.target.value);
+  };
+
+  const onAddNewCounter = () => {
+    // take the title entered
+    // create new counter obj
+    const newCounterObj = {
+      id: 3,
+      title: newCounterTitle,
+      counterValue: 0,
+    };
+    // add object to current counters without modifying original
+
+    setCounters([...counters, newCounterObj]);
+    setNewCounterTitle('');
+  };
 
   const handleIncrement = (id) => {
     console.log('increse counter with id', id);
     // take counters arr, find the one with id === id and indcrese counter value by 1
+
+    console.log(counters);
+    const countersCopy = [...counters];
+    const foundEl = countersCopy.find((el) => el.id === id);
+    foundEl.counterValue = foundEl.counterValue + 1;
+    setCounters(countersCopy);
   };
 
   return (
-    <div className='d-flex'>
-      {counters.map((counterObj) => (
-        <CounterSimple
-          key={counterObj.id}
-          counter={counterObj}
-          onIcrement={handleIncrement}
-        />
-      ))}
-    </div>
+    <>
+      <h2>Counter list</h2>
+      <input
+        value={newCounterTitle}
+        onChange={onNewCounterTitleEntry}
+        className='form-control'
+        type='text'
+        placeholder='new counter'
+      />
+      <button onClick={onAddNewCounter} className='btn btn-outline-primary'>
+        add
+      </button>
+      <div className='d-flex'>
+        {counters.map((counterObj) => (
+          <CounterSimple
+            key={counterObj.id}
+            counter={counterObj}
+            onIcrement={handleIncrement}
+          />
+        ))}
+      </div>
+    </>
   );
 }
 
